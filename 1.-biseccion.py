0b8d31ce567e2b5f7627834ae0f26b4c6fd6328e
Andre-PeraltaM <71809204+Andre-PeraltaM@users.noreply.github.com>
2020-12-27 19:38:34 -0600

    Add files via upload

---
+++ 1.-biseccion.py
@@ -0,0 +1,110 @@
+import brain
+import pandas as pd
+""" Modulo de HyperBrain para resolver operaciones por metodo de bolzano"""
+
+class Biseccion:
+	def __init__(self,funtion):
+		self.funcion = funtion#este será un encabezado de columna
+	def bolzano(self,a,b):
+		'''
+		Ecuación del teorema de Bolzano
+		Resive dos valores y devuelve el resultado
+		'''
+		return (a+b)/2
+
+	def error(self,a,b):
+		'''
+		Calcula el error de Bolzano
+		Resive dos valores y devuelve un valor 
+		'''
+		return abs((a-b)/b)
+
+	def cambioSigno(self,tablaTabulacion):
+		'''
+		Resive un dataframe para buscar un cambio de signo en la tabulacion de Y
+		regresa la posición del cambio de signo
+		'''
+		signo = '+' if tablaTabulacion['Y'][0] >= 0 else '-'
+		signoVar = ''
+		cambioS = [[],[]]
+		for i in range(len(tablaTabulacion['Y'])):
+			signoVar = '+' if tablaTabulacion['Y'][i] >= 0 else '-'
+			if signo != signoVar:
+				cambioS[0].append(tablaTabulacion['X'][i-1])
+				cambioS[0].append(signoVar)
+				cambioS[1].append(tablaTabulacion['X'][i])
+				cambioS[1].append(signoVar)
+				break
+		if cambioS[1][1] == '-' :
+			cambioS[0][1] = '+'
+		else:
+			cambioS[0][1] = '-'	
+		return cambioS
+			
+
+	def solucion(self):
+
+		funcionConstruida = brain.ecuacion(self.funcion)
+
+		listaY = brain.tabulacion(funcionConstruida)
+
+		filas =  ["X",'Y']
+		columnas = [">",">",">",">",">",">",">"]
+
+		datos = [[-3],[-2],[-1],[0],[1],[2],[3]]
+
+		for i in range(len(listaY)):
+			datos[i].append(listaY[i])
+
+		tablaTabulacion = pd.DataFrame(datos,columnas,filas) #La primera tabla 
+		x = self.cambioSigno(tablaTabulacion)
+		for i in range(2):
+			x[i].append(brain.sustitucion(funcionConstruida,value = str(x[i][0])))
+		
+		ValorAnterior = x[0][0]
+		Valoractual = x[1][0]
+		UltimoPositivo = x[1][0]
+		for i in range(1,20):
+			if x[i][1] == '+':
+				ValorAnteriorTemp = Valoractual
+				Valoractual = self.bolzano(Valoractual,ValorAnterior)
+				ValorAnterior = ValorAnteriorTemp
+				x.append([Valoractual])
+			else:
+				ValorAnteriorTemp = Valoractual
+				Valoractual = self.bolzano(Valoractual,UltimoPositivo)
+				ValorAnterior = ValorAnteriorTemp
+				x.append([Valoractual])
+
+			sustitucionActual = brain.sustitucion(funcionConstruida,value = str(x[-1][-1]) )
+
+			if sustitucionActual >= 0 :
+				x[-1].append('+')
+			else:
+				x[-1].append('-')
+			x[-1].append(sustitucionActual)
+
+			if x[-1][1] == '+':
+				UltimoPositivo = x[-1][0]
+			if len(x)  == 18:
+				break
+		f = self.funcion
+		self.funcion = f'     f(x) = {self.funcion}'
+		filas =  ["Bolzano  ",'  Signo',self.funcion]
+		columnas = ['a','b',"1","2","3","4","5","6","7",'8','9','10','11','12','13','14','15','16']
+
+		resultadosFinales = pd.DataFrame(x,columnas,filas) #La Segunda tabla
+		resultadoError = []
+		for i in range(3,len(resultadosFinales['Bolzano  '])):
+			resultadoError.append(self.error(resultadosFinales['Bolzano  '][i-1],resultadosFinales['Bolzano  '][i]))
+
+		resultadosFinales['  Error  '] = ['-','-','-'] + resultadoError
+		brain.grafica(f)
+		return tablaTabulacion,resultadosFinales
+'''
+x = Biseccion('x^4+3x^3-2')#Ejemplo
+y =x.solucion()
+
+print(y[0])
+print(y[1])
+'''
---
+++ brain.py
@@ -0,0 +1,202 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+import pylab as pl
+
+''' El cerebro de toda la libreria'''
+def prueba(x):
+	x = str(x)
+	apertura = ['(','[','{']
+	cierre = [')',']','}']
+
+	lista ={')':'(',']':'[','}':'{'}
+
+	orden = []
+
+	for i in x:
+		if i in apertura:
+			orden.append(i)
+			print('s')
+		elif i in cierre:
+			print('a')
+			if orden[-1] == lista[i]:
+				orden.pop()
+			else:
+				return False
+	if len(orden) == 0:
+
+		return True
+	else:
+
+		return False
+
+def comprobar(funcion):
+
+	x = prueba(funcion)
+
+	if x == False:
+		return False
+	else:
+		return True
+
+
+
+
+
+def sic(x):
+	return 1/math.cos(x)
+
+def csc(x):
+	return 1/math.sin(x)
+
+def cot(x):
+	return 1/math.tan(x)
+
+
+
+def grafica(function):
+	'''
+	Tabula y grafica una función  
+	'''
+	
+	x = np.linspace(-10,3,100)
+
+	x_str = list(map(str,np.linspace(-10,3,100)))
+	y = []
+	for i in x_str:
+		y.append(sustitucion(function,i))
+
+	figura = plt.figure(figsize=(7,7),edgecolor = 'black') 
+	grafico = figura.add_axes([0.1,0.1,0.9,0.9])
+	grafico.plot(x,y ,'red',label = f"f(x)={function}")
+	grafico.legend(loc = 0)
+
+	plt.title(f" f(x) = :  {function} ") 
+	ax = pl.gca()  # gca stands for 'get current axis'
+	ax.spines['right'].set_color('none')
+	ax.spines['top'].set_color('none')
+	ax.xaxis.set_ticks_position('bottom')
+	ax.spines['bottom'].set_position(('data',0))
+	ax.yaxis.set_ticks_position('left')
+	ax.spines['left'].set_position(('data',0))
+
+	plt.xlabel("Eje X")
+
+	plt.ylabel("Eje Y")
+	plt.show()
+	
+
+def __parentesis(function,caracter):
+	#ESTE METODO DEBE SER PRIVADO
+	if caracter == '(':
+		sustitucion = '*('
+		contra = ')'
+		x = -1
+		y = 0
+	if caracter == ')':
+		sustitucion = ')*'
+		contra = '('
+		x = +1
+		y = 1
+
+	if caracter in function:
+		lugar_caracter = function.find(caracter)+1
+		numero_repeticiones = function.count(caracter)
+		s = 0
+		for i in range(numero_repeticiones):
+			cacho_cadena = function[s:lugar_caracter + y]
+			posicion = cacho_cadena.find(caracter)
+			try:
+				if cacho_cadena[posicion+ x].isdecimal() or cacho_cadena[posicion+ x] == contra:
+					cacho_cadena = cacho_cadena.replace(caracter,sustitucion)
+			except IndexError:
+				break
+			function = function.replace(function[s:lugar_caracter+y],cacho_cadena)
+			s = lugar_caracter+1
+			lugar_caracter = function[s:].find(caracter)+1+s
+
+	return function
+
+def ecuacion(function):
+	'''
+	Sustituye los valores de una ecuacion en su forma str para hacerla valida por el metodo eval()
+	Resive un valor Str y devuelve un valor str equivalente que puede ser introducido en eval()
+	Si la función está mal entonces se devuelve 'error'
+	'''
+	
+
+	tabla_de_valores = { 88: '(x)', 120: '(x)', 94: '**'}
+	function = function.translate(tabla_de_valores)
+	#Las anteriores 3 lineas son para sustituir X, x y ^
+	diccionario_otras_variables = {'sec':'(sic','csc':'(csc','cot':'(cot','sen':'(math.sin','cos':'(math.cos','tan':'(math.tan','senh':'(math.sinh','cosh':'(math.cosh','tanh':'(math.tanh','sin^-1': '(math.asin', 'cos^-1' : '(math.acos' , 'tan^-1' : '(math.atan','π':'(math.pi)','sqrt':'(math.sqrt','√':'(math.sqrt','Ln':'PROCESO','PROCESO':'(math.log10','PROCESO':'(math.log','PROCESO':'(math.acos','PROCESO':'(math.asin','PROCESO':'(math.atan','e':'(math.e)'}
+
+	#math.log(x, base)
+	for i in diccionario_otras_variables:
+		if i in function:
+			for j in range(function.count(i)):
+				z = function[function.find(i)+len(i)+1:]
+				function = function[:function.find(i)+len(i)+1+z.find(')')] + '))' + function [function.find(i)+len(i)+1+z.find(')')+1:] 
+				function = function.replace(i,diccionario_otras_variables[i],1)
+
+
+	function = __parentesis(function,'(')#Hace que los parentesis multipliquen
+	function = __parentesis(function,')')
+	
+	#Valor absoluto
+	return function
+def sustitucion(function, value = '1'):
+	'''
+	Sustituye los valores de X en una ecuación por el valor dado
+	Si no se da un valor se evalua en 1 
+	'''
+	function = ecuacion(function)
+	function = function.replace('x',value)
+	try:
+		return eval(function)
+	except ZeroDivisionError:
+		return None
+	except ValueError:
+		return None
+
+
+def tabulacion(function,value1 = -3,value2 = 3):
+	'''
+	Tabula una función tipo eval() en un rango de valores.
+	Recibe una función str y dos valores que son los limites, para devolver una lista con los resultados
+	'''
+	valuesX = list(map(str,range(value1,value2+1)))
+
+	rango_valoresY = []
+
+	for i in valuesX:
+		rango_valoresY.append(sustitucion(function,i)) 
+
+	return rango_valoresY
+
+def hay(un_objeto , en_este_argumento , y_este_aparece_solo = 1):
+	'''
+	Usando un if se lee de la siguiente manera:
+
+	Si hay un objeto (x) en un argumento (y) , devuelve true si este elemento está presente éste numero de veces (z)
+
+	Funcion que retorna True si un objeto está presente en un argumento (listas,tuplas,str etc.)
+	Si le aplica el tercer argumento entonces se le puede pedir que nos diga si está presente el número de veces que queremos
+	Si como tercer argumento se le pone un 0 entonces estamos diciendo que nos diga si no está
+
+
+	'''
+	cont = 0
+	for i in en_este_argumento:
+		if un_objeto == i:
+			cont += 1
+
+	if y_este_aparece_solo == cont:
+		return True
+	else:
+		return False
+
+
+#AQUI EMPEZAMOS CON LA CALCULADORA
+
+
+
